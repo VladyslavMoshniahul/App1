1c0318e276da64e6c0a6d1535bafffe085a88664
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-07-12 18:07:16 +0300

    add a school and class logic

--- demo/src/main/java/com/example/demo/javaSrc/school/ClassRepository.java
+++ demo/src/main/java/com/example/demo/javaSrc/school/ClassRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.javaSrc.school;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClassRepository extends JpaRepository<SchoolClass, Long> {
+    List<SchoolClass> findBySchoolId(Long schoolId);
     
 }
