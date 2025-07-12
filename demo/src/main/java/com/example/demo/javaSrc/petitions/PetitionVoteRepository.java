814525757447cea91210ad03b46849a23322c063
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-07-12 18:31:19 +0300

    add a petitions logic

--- demo/src/main/java/com/example/demo/javaSrc/petitions/PetitionVoteRepository.java
+++ demo/src/main/java/com/example/demo/javaSrc/petitions/PetitionVoteRepository.java
@@ -2,4 +2,5 @@ package com.example.demo.javaSrc.petitions;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetitionVoteRepository extends JpaRepository<PetitionVote, Long> {
+    boolean existsByPetitionIdAndStudentId(Long petitionId, Long studentId);
 }
