3554ab3e76b21349691c4b4a4ccb4385b29ba260
VladyslavMoshniahul <163896225+VladyslavMoshniahul@users.noreply.github.com>
2025-07-12 13:54:27 +0300

    mini fix

--- demo/src/main/java/com/example/demo/App1.java
+++ demo/src/main/java/com/example/demo/App1.java
@@ -1,9 +1,9 @@
-package main.java.com.example.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "main.java.com.example.demo")
+@SpringBootApplication(scanBasePackages = "com.example.demo")
 public class App1 {
     public static void main(String[] args) {
         SpringApplication.run(App1.class, args);
